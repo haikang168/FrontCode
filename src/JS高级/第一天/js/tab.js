44925f72085b9dd8928b40720f1396c4d3bee6cc
haikang <1584841572@qq.com>
2022-09-14 20:48:03 +0800

    JS高级复习

---
+++ src/JS高级/第一天/js/tab.js
@@ -0,0 +1,21 @@
+// 定义tab类
+class Tab{
+    // 定义构造器
+    constructor(name) {
+
+    }
+
+    // 初始化init方法
+
+    // 添加方法
+    addTab(){}
+
+    // 修改方法
+    updateTab(){}
+
+    // 删除方法
+    removeTab(){}
+
+    // 切换方法
+    toggleTab(){}
+}
--- src/MyDate.java
+++
@@ -1,9 +0,0 @@
-/**
- * @Author 海康
- * @Version 1.0
- */
-public class MyDate {
-    public static void main(String[] args) {
-        System.out.println(System.currentTimeMillis());
-    }
-}
